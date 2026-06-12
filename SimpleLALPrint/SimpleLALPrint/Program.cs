daf09fd10ff8a99aefbd9c5fbf3202ed47f9c01f
JonMation <61788149+JonMation@users.noreply.github.com>
2026-06-12 10:58:11 +0200

    Refactor into separate classes

--- SimpleLALPrint/SimpleLALPrint/Program.cs
+++ SimpleLALPrint/SimpleLALPrint/Program.cs
@@ -2,10 +2,10 @@
 using combit.Reporting.DataProviders;
 using combit.Reporting.Dom;
 using System.Data;
+using SimpleLALPrint;
 
-DataTable table = new DataTable("Texts");
-table.Columns.Add("Text", typeof(string));
-table.Rows.Add("Hello World");
+DataTable table = ReportDataFactory.CreateHelloWorldTable();
+string fileName = "hello";
 
 string projectFolder = Path.GetFullPath(
     Path.Combine(AppContext.BaseDirectory, @"..\..\..\")
@@ -14,47 +14,10 @@ string projectFolder = Path.GetFullPath(
 string reportsFolder = Path.Combine(projectFolder, "reports");
 Directory.CreateDirectory(reportsFolder);
 
-string projectFile = Path.Combine(reportsFolder, "hello.lst");
-string outputPdf = Path.Combine(reportsFolder, "hello.pdf");
-
 using ListLabel ll = new ListLabel();
 ll.DataSource = new AdoDataProvider(table);
 
-// 1. hello.lst automatisch per Code erstellen
-
-ProjectList project = new ProjectList(ll);
-
-project.Open(
-    projectFile,
-    LlDomFileMode.Create,
-    LlDomAccessMode.ReadWrite
-);
-
-ObjectText textObject = new ObjectText(project.Objects);
-textObject.Name = "HelloWorldText";
-textObject.Position.Set(20000, 20000, 120000, 20000);
-
-Paragraph paragraph = new Paragraph(textObject.Paragraphs);
-paragraph.Contents = "\"Hello World das funktiniert super\"";
-
-project.Save();
-project.Close();
-
-Console.WriteLine(".lst erstellt:");
-Console.WriteLine(projectFile);
-
-// if (!File.Exists(projectFile)) { }
-
-// 2. PDF aus der automatisch erstellten .lst erzeugen
-var exportConfig = new ExportConfiguration(
-    LlExportTarget.Pdf,
-    outputPdf,
-    projectFile
-);
-
-ll.Export(exportConfig);
-
-Console.WriteLine(".pdf erstellt:");
-Console.WriteLine(outputPdf);
+TemplateCreator.Create(ll, reportsFolder, fileName);
+ReportPdfExporter.ExportPDF(ll, reportsFolder, fileName);
 
 Console.ReadLine();
---
+++ SimpleLALPrint/SimpleLALPrint/ReportPdfExporter.cs
@@ -0,0 +1,24 @@
+using combit.Reporting;
+
+namespace SimpleLALPrint;
+
+public class ReportPdfExporter
+{
+    // 2. PDF aus der automatisch erstellten .lst erzeugen
+    public static void ExportPDF(ListLabel ll, string reportsFolder, string fileName)
+    {
+        string projectFile = Path.Combine(reportsFolder, fileName + ".lst");
+        string outputPdf = Path.Combine(reportsFolder, fileName + ".pdf");
+
+        var exportConfig = new ExportConfiguration(
+            LlExportTarget.Pdf,
+            outputPdf,
+            projectFile
+        );
+
+        ll.Export(exportConfig);
+        
+        Console.WriteLine(".pdf erstellt:");
+        Console.WriteLine(outputPdf);
+    }
+}
